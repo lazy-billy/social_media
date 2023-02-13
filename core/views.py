20470298a95e7f21bbec43966c49f54de3aa94c3
lazy-billy <dizzy95@list.ru>
2023-02-13 21:09:04 +0500

    ...

--- core/views.py
+++ core/views.py
@@ -2,13 +2,47 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 from .models import Profile
 
 # Create your views here.
 
 
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
+
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        return redirect('settings')
+    
+    return render(request, 'setting.html', {'user_profile': user_profile})
 
 
 def signup(request):
@@ -32,6 +66,9 @@ def signup(request):
                 user.save()
 
                 # Log user in and redirect to settings page
+                user_login = auth.authenticate(
+                    username=username, password=password)
+                auth.login(request, user_login)
 
                 # Create a Profile object for the new user
                 user_model = User.objects.get(username=username)
@@ -49,7 +86,7 @@ def signup(request):
 
 
 def signin(request):
-    
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -66,6 +103,7 @@ def signin(request):
     return render(request, 'signin.html')
 
 
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
