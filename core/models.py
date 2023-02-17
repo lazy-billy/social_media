ce46b745e98f35528dc9310d6c77faa2b5555ff1
lazy-billy <dizzy95@list.ru>
2023-02-17 23:44:52 +0500

    added following people funtionality

--- core/models.py
+++ core/models.py
@@ -10,12 +10,13 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
+    profileimg = models.ImageField(
+        upload_to='profile_images', default='blank-profile-picture.png')
     location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return self.user.username
-    
+
 
 class Post(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
@@ -27,7 +28,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.user
-    
+
 
 class LikePost(models.Model):
     post_id = models.CharField(max_length=500)
@@ -35,3 +36,11 @@ class LikePost(models.Model):
 
     def __str__(self):
         return self.username
+
+
+class FollowersCount(models.Model):
+    follower = models.CharField(max_length=100)
+    user = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user
